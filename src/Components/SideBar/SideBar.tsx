673502b3f147acb74649894a1933832595d2e58f
mariameid <mariameid210@gmail.com>
2025-08-12 09:48:52 +0200

    added collapsing property in sidebr

--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -1,17 +1,46 @@
 import { CgProfile } from "react-icons/cg";
 import { CiLogout } from "react-icons/ci";
-import { FaRegUser } from "react-icons/fa";
+import {
+  FaArrowCircleLeft,
+  FaArrowCircleRight,
+  FaRegUser,
+} from "react-icons/fa";
 import { IoHomeOutline } from "react-icons/io5";
 import { LuUsers } from "react-icons/lu";
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function SideBar() {
+  const [collapsed, setCollapsed] = useState(false);
+  const toggleCollapse = () => {
+    setCollapsed(!collapsed);
+    //this means that
+    // if (collapsed) {
+    //   setCollapsed(false);
+    // } else {
+    //   setCollapsed(true);
+    // }
+  };
   return (
     <div className="sidebarContainer vh-100">
-      <Sidebar className="h-100">
+      <Sidebar collapsed={collapsed} className="h-100">
+        {collapsed ? (
+          <FaArrowCircleLeft
+            size={25}
+            className="m-3"
+            onClick={toggleCollapse}
+          />
+        ) : (
+          <FaArrowCircleRight
+            size={25}
+            className="m-3"
+            onClick={toggleCollapse}
+          />
+        )}
+
         <div className="text-center my-3">
-          <img src="" className="rounded-circle" alt="profile img" />
+          <img src="" className="rounded-circle w-75" alt="profile img" />
           <h5>Mariam</h5>
           <h6 className="text-warning">Admin</h6>
         </div>
