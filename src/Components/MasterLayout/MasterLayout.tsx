88e8bb2f983c93dce8657e2ad9b79b48896ccc75
mariameid <mariameid210@gmail.com>
2025-08-12 07:59:42 +0200

    added sidebar Ui

--- src/Components/MasterLayout/MasterLayout.tsx
+++ src/Components/MasterLayout/MasterLayout.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 import SideBar from "../SideBar/SideBar";
 import NavBar from "../NavBar/NavBar";
@@ -6,15 +5,13 @@ import NavBar from "../NavBar/NavBar";
 export default function MasterLayout() {
   return (
     <>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-3">
-            <SideBar />
-          </div>
-          <div className="col-md-9">
-            <NavBar />
-            <Outlet />
-          </div>
+      <div className="d-flex">
+        <div>
+          <SideBar />
+        </div>
+        <div className="w-100 ">
+          <NavBar />
+          <Outlet />
         </div>
       </div>
     </>
--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -1,5 +1,49 @@
-import React from "react";
+import { CgProfile } from "react-icons/cg";
+import { CiLogout } from "react-icons/ci";
+import { FaRegUser } from "react-icons/fa";
+import { IoHomeOutline } from "react-icons/io5";
+import { LuUsers } from "react-icons/lu";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { Link } from "react-router-dom";
 
 export default function SideBar() {
-  return <div>SideBar</div>;
+  return (
+    <div className="sidebarContainer vh-100">
+      <Sidebar className="h-100">
+        <div className="text-center my-3">
+          <img src="" className="rounded-circle" alt="profile img" />
+          <h5>Mariam</h5>
+          <h6 className="text-warning">Admin</h6>
+        </div>
+        <Menu>
+          <MenuItem
+            icon={<IoHomeOutline />}
+            component={<Link to="/dashboard" />}
+          >
+            Home
+          </MenuItem>
+          <MenuItem
+            icon={<LuUsers />}
+            component={<Link to="/dashboard/users-list" />}
+          >
+            Users
+          </MenuItem>
+          <MenuItem
+            icon={<FaRegUser />}
+            component={<Link to="/dashboard/add-user" />}
+          >
+            Add User
+          </MenuItem>
+
+          <MenuItem
+            icon={<CgProfile />}
+            component={<Link to="/dashboard/profile" />}
+          >
+            Profile
+          </MenuItem>
+          <MenuItem icon={<CiLogout />}>Logout</MenuItem>
+        </Menu>
+      </Sidebar>
+    </div>
+  );
 }
